dde37881243b0ecfc9ac374abedccc26a432a47c
TylorSwift2 <roberdoogarcia@gmail.com>
2025-06-12 20:19:06 -0300

    add home page

--- src/core/models.py
+++ src/core/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 class Pessoa(models.Model):
     user = models.CharField(max_length=100)
-    senha = models.IntegerField()
+    senha = models.CharField(max_length=255)
 
     @classmethod
     def registrer(cls, user, senha):
+        hashed_password = make_password(senha) 
         pessoa = cls(user=user, senha=senha)
         pessoa.save()
         return pessoa
         
     def __str__(self):
-        return self.user
+        return self.user, self.senha
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path # Removed unused imports
-from core.core.views.views import home
+from core.core.views.views import home, registro
 urlpatterns = [
     path('', home, name="home"),
+    path('registrer/', registro, name="registrer")
 ]
